aa239e9484fd71013aec3521f0e22020370fca7c
JbCoder <JbCoder@users.noreply.github.com>
2019-01-06 18:13:35 +0000

    ambiguity of variable "c" removed

--- QuizGen.py
+++ QuizGen.py
@@ -3,10 +3,9 @@
 # Contributions to this code are welcome! Go to https://github.com/JbCoder/PyQuiz
 # Make sure to add your GitHub username to the contributors.txt file.
 import random, os
-from threading import Thread
 def quiz(number_of_questions):
 	# setup
-	c = 0
+	number_of_questions_done = 0
 	score = 0
 	questions_printed = []
 	i = open("Questions.txt", "r")
@@ -17,11 +16,11 @@ def quiz(number_of_questions):
 	answers = lines[1::2]
 	print("Quiz Generated!")
 	print("You will be asked " + str(number_of_questions) + " questions. Good Luck!")
-	while c < number_of_questions:
+	while number_of_questions_done < number_of_questions:
 		current_question = random.randint(-1, len(questions)-1)
 		if current_question in questions_printed:
 			# if the randomly selected question has already been asked then find a different questions to ask. 
-			c += -1
+			continue
 		else:
 			questions_printed.append(current_question)
 			# add the current question to list of done questions to avoid repetition
@@ -31,7 +30,7 @@ def quiz(number_of_questions):
 				score += 1
 			else:
 				print("Incorrect!")
-		c += 1
+			number_of_questions_done += 1
 	print("You scored " + str(score) + "/" + str(number_of_questions))
 	os.system("pause")
 	#print(questions)
