2ab9df3c3b0c60604ebd4954ab3bf3929cca607e
JbCoder <JbCoder@users.noreply.github.com>
2019-01-06 18:09:20 +0000

    Quiz is now in a function

--- QuizGen.py
+++ QuizGen.py
@@ -3,34 +3,37 @@
 # Contributions to this code are welcome! Go to https://github.com/JbCoder/PyQuiz
 # Make sure to add your GitHub username to the contributors.txt file.
 import random, os
-# setup
-c = 0
-score = 0
-number_of_questions = 6
-questions_printed = []
-i = open("Questions.txt", "r")
-i = i.read()
-lines = i.split("\n")
-questions = lines[::2]
-total_questions = len(questions)
-answers = lines[1::2]
-print("Quiz Generated!")
-print("You will be asked " + str(number_of_questions) + " questions. Good Luck!")
-while c < number_of_questions:
-	current_question = random.randint(-1, len(questions)-1)
-	if current_question in questions_printed:
-		# if the randomly selected question has already been asked then find a different questions to ask. 
-		c += -1
-	else:
-		questions_printed.append(current_question)
-		ans = input(questions[current_question]).lower()
-		if ans == answers[current_question].lower():
-			print("Correct!")
-			score += 1
+from threading import Thread
+def quiz(number_of_questions):
+	# setup
+	c = 0
+	score = 0
+	questions_printed = []
+	i = open("Questions.txt", "r")
+	i = i.read()
+	lines = i.split("\n")
+	questions = lines[::2]
+	total_questions = len(questions)
+	answers = lines[1::2]
+	print("Quiz Generated!")
+	print("You will be asked " + str(number_of_questions) + " questions. Good Luck!")
+	while c < number_of_questions:
+		current_question = random.randint(-1, len(questions)-1)
+		if current_question in questions_printed:
+			# if the randomly selected question has already been asked then find a different questions to ask. 
+			c += -1
 		else:
-			print("Incorrect!")
-	c += 1
-print("You scored " + str(score) + "/" + str(number_of_questions))
-os.system("pause")
-#print(questions)
-#print(answers)
+			questions_printed.append(current_question)
+			# add the current question to list of done questions to avoid repetition
+			ans = input(questions[current_question]).lower()
+			if ans == answers[current_question].lower():
+				print("Correct!")
+				score += 1
+			else:
+				print("Incorrect!")
+		c += 1
+	print("You scored " + str(score) + "/" + str(number_of_questions))
+	os.system("pause")
+	#print(questions)
+	#print(answers)	
+quiz(6)
