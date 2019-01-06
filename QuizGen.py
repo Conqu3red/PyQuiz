c4ec5863c97501fdf869c24b60947f4d69b2da2b
JbCoder <JbCoder@users.noreply.github.com>
2019-01-06 17:52:20 +0000

    Code Explainations
    v1

--- QuizGen.py
+++ QuizGen.py
@@ -1,3 +1,7 @@
+# Written to work on Python 3.7.1 (v3.7.1:260ec2c36a, Oct 20 2018, 14:05:16)
+# Code Writen by JbCoder (GitHub)
+# Contributions to this code are welcome! Go to https://github.com/JbCoder/PyQuiz
+# Make sure to add your GitHub username to the contributors.txt file.
 import random, os
 c = 0
 score = 0
@@ -14,6 +18,7 @@ print("You will be asked " + str(number_of_questions) + " questions. Good Luck!"
 while c < number_of_questions:
 	current_question = random.randint(-1, len(questions)-1)
 	if current_question in questions_printed:
+		# if the randomly selected question has already been asked then find a different questions to ask. 
 		c += -1
 	else:
 		questions_printed.append(current_question)
